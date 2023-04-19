1199d6d23bfcc3c139671ae5b5d2d4f04c9565ac
m3211n <130044007+m3211n@users.noreply.github.com>
2023-04-20 00:05:41 +0200

    Update pxt.json, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,42 +1,44 @@
 sprites.onCreated(SpriteKind.Player, function (sprite) {
-    success = false
-    while (!(success)) {
-        randomC = randint(1, 8)
-        randomR = randint(1, 6)
-        if (tiles2.length > 0) {
-            for (let sprite of grid.allSprites()) {
-                if (grid.spriteCol(sprite) != randomC || grid.spriteRow(sprite) != randomR) {
+    let randomC = randint(1, 8)
+    let randomR = randint(1, 6)
+    if (tilesSprites.length > 0) {
+        let success = false
+        while (!(success)) {
+            for (let sprite of tilesSprites) {
+                if (sprite.tilemapLocation().column != randomC && sprite.tilemapLocation().row != randomR) {
                     success = true
                 }
             }
-        } else {
-            success = true
+            randomC = randint(1, 8)
+            randomR = randint(1, 6)
         }
     }
-    grid.place(sprite, tiles.getTileLocation(randomC, randomR))
     sprites.setDataBoolean(sprite, "updated", false)
     sprites.setDataNumber(sprite, "rank", 0)
+    sprites.setDataNumber(sprite, "col", randomC)
+    sprites.setDataNumber(sprite, "row", randomR)
 })
-let randomR = 0
-let randomC = 0
-let success = false
-let tiles2: Sprite[] = []
-let images2 = [
-assets.image`tile_0`,
-assets.image`tile_1`,
-assets.image`tile_2`,
-assets.image`tile_3`,
-assets.image`tile_4`,
-assets.image`tile_5`,
-assets.image`tile_6`,
-assets.image`tile_7`,
-assets.image`tile_8`,
-assets.image`tile_9`
+
+let tilesSprites: Sprite[] = []
+
+let tilesImages = [
+    assets.image`tile_0`,
+    assets.image`tile_1`,
+    assets.image`tile_2`,
+    assets.image`tile_3`,
+    assets.image`tile_4`,
+    assets.image`tile_5`,
+    assets.image`tile_6`,
+    assets.image`tile_7`,
+    assets.image`tile_8`,
+    assets.image`tile_9`
 ]
-tiles2 = []
+
 scene.centerCameraAt(80, 64)
 tiles.setCurrentTilemap(tilemap`level1`)
-tiles2.push(sprites.create(img`
+
+for (let i = 0; i < 48; i++) {
+    tilesSprites.push(sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -54,31 +56,17 @@ tiles2.push(sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player))
-tiles2.push(sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Player))
+
+}
+
 game.onUpdate(function () {
-    for (let sprite of tiles2) {
+    for (let sprite of tilesSprites) {
         if (!(sprites.readDataBoolean(sprite, "updated"))) {
-            sprite.setImage(images2[sprites.readDataNumber(sprite, "rank")])
+            sprite.setImage(tilesImages[sprites.readDataNumber(sprite, "rank")])
             sprites.setDataBoolean(sprite, "updated", true)
+            tiles.placeOnTile(sprite, tiles.getTileLocation(sprites.readDataNumber(sprite, "col"), sprites.readDataNumber(sprite, "row")))
         }
-        if (sprites.readDataNumber(sprite, "rank") == 9) {
+        if (sprites.readDataNumber(sprite, "rank") == 10) {
             pause(500)
             game.gameOver(true)
         }
