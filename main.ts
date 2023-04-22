5015bcaa5101b3ce262f2f1abf88cee1bb033270
m3211n <130044007+m3211n@users.noreply.github.com>
2023-04-22 03:57:56 +0200

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -14,7 +14,7 @@ sprites.onCreated(SpriteKind.Player, function (sprite) {
     let freeCell = freeCells._pickRandom()
 
     sprites.setDataBoolean(sprite, "updated", false)
-    sprites.setDataNumber(sprite, "rank", Math.randomRange(0, 5))
+    sprites.setDataNumber(sprite, "rank", 0)
     grid.place(sprite, tiles.getTileLocation(freeCell % gridW, Math.floor(freeCell / gridW) + 1))
 })
 
@@ -44,78 +44,81 @@ function seed(count: number) {
     }
 }
 
-function mergeTiles(segment: Sprite[], reversed: boolean) {
-    let s = " | "
+function mergeTiles(segment: Sprite[], reversed: boolean, horizontal: boolean) {
+    // merge
     for (let i = 0; i < segment.length - 1; i++) {
         let index = reversed ? segment.length - 1 - i : i
         let next = reversed ? index - 1 : index + 1
-        s = s + sprites.readDataNumber(segment[index], "rank") + "<>" + sprites.readDataNumber(segment[next], "rank") + " | "
         if (sprites.readDataNumber(segment[index], "rank") == sprites.readDataNumber(segment[next], "rank")) {
             sprites.changeDataNumberBy(segment[index], "rank", 1)
             sprites.setDataBoolean(segment[index], "updated", false)
+            segment.removeElement(segment[next])
             sprites.destroy(segment[next])
         }
     }
-    console.log(s)
-}
 
-function moveTiles(tiles: Sprite[], reversed: boolean, vertical: boolean) {
+    // move
+    let edgeIndex = reversed ? (horizontal ? gridW - 1 : gridH) : (horizontal ? 0 : 1)
+    let inc = reversed ? -1 : 1
+    for (let item of segment) {
+        if (horizontal) {
+            let row = grid.spriteRow(item)
+            let col = grid.spriteCol(item)
+            console.log("col " + col + " / edgeIndex" + edgeIndex)
+            if (col != edgeIndex) {
+                moved = true
+                console.log("Moved!")
+            }
+            grid.place(item, tiles.getTileLocation(edgeIndex, row))
+        } else {
+            let row = grid.spriteRow(item)
+            let col = grid.spriteCol(item)
+            console.log("row " + row + " / edgeIndex" + edgeIndex)
+            if (row != edgeIndex) {
+                moved = true
+                console.log("Moved!")
+            }
+            grid.place(item, tiles.getTileLocation(col, edgeIndex))
+        }
+        edgeIndex += inc
+    }
 }
 
 function changeGravityVector(vector: number) {
-
-    console.log("== INITIAL ==")
-
-    for (let col = 0; col < grid.numColumns(); col++) {
-        let s = "col " + col + ": | "
-        for (let sprite of grid.colSprites(col)) {
-            s = s + sprites.readDataNumber(sprite, "rank") + " | "
-        }
-        console.log(s)
-    }
-
-    console.log("----------------")
-
-    for (let row = 0; row < grid.numRows(); row++) {
-        let s = "row " + row + ": | "
-        for (let sprite of grid.rowSprites(row)) {
-            s = s + sprites.readDataNumber(sprite, "rank") + " | "
-        }
-        console.log(s)
-    }
-
+    console.log("== MERGING AND MOVING ==")
+    let shallSeed: boolean = false
     switch (vector) {
         case 9:             // LEFT
             for (let row = 0; row < grid.numRows(); row++) {
-                console.log("row " + row + ":")
-                mergeTiles(grid.rowSprites(row), false)
+                // console.log("row " + row + ":")
+                mergeTiles(grid.rowSprites(row), false, true)
             }
             break
         
         case 3:             // RIGHT
             for (let row = 0; row < grid.numRows(); row++) {
-                console.log("row " + row + ":")
-                mergeTiles(grid.rowSprites(row), true)
+                // console.log("row " + row + ":")
+                mergeTiles(grid.rowSprites(row), true, true)
             }
             break
         
         case 0:             // UP
             for (let col = 0; col < grid.numColumns(); col++) {
-                console.log("col " + col + ":")
-                mergeTiles(grid.colSprites(col), false)
+                // console.log("col " + col + ":")
+                mergeTiles(grid.colSprites(col), false, false)
             }
             break
 
         case 6:             // DOWN
             for (let col = 0; col < grid.numColumns(); col++) {
-                console.log("col " + col + ":")
-                mergeTiles(grid.colSprites(col), true)
+                // console.log("col " + col + ":")
+                mergeTiles(grid.colSprites(col), true, false)
             }
             break
-
     }
+}
 
-    console.log("== RESULT ==")
+/*  console.log("== RESULT ==")
 
     for (let col = 0; col < grid.numColumns(); col ++) {
         let s = "col " + col + ": | "
@@ -135,11 +138,13 @@ function changeGravityVector(vector: number) {
         console.log(s)
     }
 
-}
+    console.log("== MOVE FINISHED ==")
+}*/
 
 let tilesSprites: Sprite[] = []
 let gridW = 10
 let gridH = 6
+let moved: boolean = false
 
 let tilesImages = [
     assets.image`tile_0`,
@@ -160,7 +165,29 @@ info.setBorderColor(0)
 info.setBackgroundColor(0)
 info.setFontColor(2)
 
-seed(20)
+seed(4)
+
+/*
+console.log("== START ==")
+
+for (let col = 0; col < grid.numColumns(); col++) {
+    let s = "col " + col + ": | "
+    for (let sprite of grid.colSprites(col)) {
+        s = s + sprites.readDataNumber(sprite, "rank") + " | "
+    }
+    console.log(s)
+}
+
+console.log("----------------")
+
+for (let row = 0; row < grid.numRows(); row++) {
+    let s = "row " + row + ": | "
+    for (let sprite of grid.rowSprites(row)) {
+        s = s + sprites.readDataNumber(sprite, "rank") + " | "
+    }
+    console.log(s)
+}
+*/
 
 game.onUpdateInterval(10, function() {
     for (let sprite of tilesSprites) {
@@ -174,6 +201,11 @@ game.onUpdateInterval(10, function() {
             game.gameOver(true)
         }
     }
+    if (moved) {
+        seed(2)
+        moved = false
+    }
+
 })
 
 controller.left.onEvent(ControllerButtonEvent.Pressed, function() {
